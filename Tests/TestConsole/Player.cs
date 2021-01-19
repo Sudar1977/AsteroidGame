6acd16e8e01124503231dd8e29e8cd5bbc2b3864
Sudar1977 <Sudar1977@gmail.com>
2021-01-19 14:59:01 +0300

    Рефакторинг

---
+++ Tests/TestConsole/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Player
+    {
+        private string _Name;
+        private DateTime _Birthday;
+        public Player()
+        {
+
+        }
+        public Player(string Name)
+        {
+            this._Name = Name;
+            _Birthday = DateTime.Now;
+        }
+        public Player(string Name, DateTime Birthday) : this(Name)
+        {
+            //this.Name = Name;
+            this._Birthday = Birthday;
+        }
+
+        public string GetName()
+        {
+            return _Name;
+        }
+
+        public void SetName(string Name)
+        {
+            _Name = Name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            //set
+            //{
+            //    _Name = value;
+            //}
+        }
+
+        public string Surname { get; } = "";// set; }
+
+    }
+}
