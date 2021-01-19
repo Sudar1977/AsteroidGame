60b29d4d11d3744b7ca5f62e5ad4f7b5b0cffe73
Sudar1977 <Sudar1977@gmail.com>
2021-01-19 16:59:43 +0300

    Пример работы с культурой

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace TestConsole
             //Player player1 = new Player();
             //player1.Name = "Иванов";
             //player1.Birthday = new DateTime(1977, 03, 23, 9, 0, 0);
+#if false
             Console.Write("Введите фамилию >");
             var surname = Console.ReadLine();
 
@@ -21,6 +23,20 @@ namespace TestConsole
 
             Console.WriteLine(player1.GetName());
             Console.WriteLine(player1.Name);
+#endif
+            var v1 = new Vector2D(5, 7);
+            var v2 = new Vector2D(-7, 2);
+            var v3 = v1 + v2;
+            var v4 = v3 + 3.1459265358979;
+
+            CultureInfo ru = new CultureInfo("ru-ru");
+            CultureInfo en_us = new CultureInfo("en-us");
+
+            double pi = double.Parse("3.1459265358979",en_us);
+            int i = (int)pi;
+
+            double lenght = v4;
+
             Console.ReadLine();
         }
     }
