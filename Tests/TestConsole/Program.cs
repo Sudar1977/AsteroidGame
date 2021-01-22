b5027935ac4bae49fd8fd10b1959dd5fc9fde1ba
Sudar1977 <Sudar1977@gmail.com>
2021-01-22 09:50:18 +0300

    Пример наследования и полиморфизма

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -24,6 +24,8 @@ namespace TestConsole
             Console.WriteLine(player1.GetName());
             Console.WriteLine(player1.Name);
 #endif
+
+#if false
             var v1 = new Vector2D(5, 7);
             var v2 = new Vector2D(-7, 2);
             var v3 = v1 + v2;
@@ -36,6 +38,23 @@ namespace TestConsole
             int i = (int)pi;
 
             double lenght = v4;
+#endif
+            Printer printer = new Printer();
+            PrefixPrinter prefixPrinter = new PrefixPrinter();
+            prefixPrinter.Prefix = "!!!!!!!-------!!!!!!!";
+            prefixPrinter.Print("QWE");
+
+            printer.Print("Hello World!");
+            prefixPrinter.PrintData(3.14);
+
+            printer.Print("123");
+
+            printer = prefixPrinter;
+
+            Printer printer1 = new PrefixPrinter();
+
+            printer.Print("345");
+            printer1.Print("678");
 
             Console.ReadLine();
         }
