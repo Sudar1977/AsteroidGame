6bf7128dc27dbb8915612ae8538e3407676284bd
Sudar1977 <Sudar1977@gmail.com>
2021-01-22 10:23:03 +0300

    Создали игровое оконо

--- Tests/TestConsole/Vector2D.cs
+++ Tests/TestConsole/Vector2D.cs
@@ -51,4 +51,25 @@ namespace TestConsole
         }
 
     }
+
+    public struct Vector2Dstruct
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Lenght
+        {
+            get
+            {
+                return Math.Sqrt(X * X + Y * Y);
+            }
+        }
+        public double Angle => Math.Atan2(Y, X);
+
+        public Vector2Dstruct(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
 }
