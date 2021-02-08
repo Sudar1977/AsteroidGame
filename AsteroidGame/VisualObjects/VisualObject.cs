5e2576283d435909b26c8e5f616f6b3012262d9b
Sudar1977 <Sudar1977@gmail.com>
2021-02-08 13:33:54 +0300

    Рефакторинг кода - перемещение визуальных объектов в отдельную папку

--- AsteroidGame/VisualObjects/VisualObject.cs
+++ AsteroidGame/VisualObjects/VisualObject.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AsteroidGame
+namespace AsteroidGame.VisualObjects
 {
     internal abstract class VisualObject
     {
