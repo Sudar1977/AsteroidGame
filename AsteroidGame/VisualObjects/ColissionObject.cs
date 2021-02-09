7fb57081429c188f02436b26ffe6c5be9ef08e29
Sudar1977 <Sudar1977@gmail.com>
2021-02-09 15:59:24 +0300

    Интерфейс проверки визуальных объектов на пересечение + реализация его базового функционала в абстрактном классе

---
+++ AsteroidGame/VisualObjects/ColissionObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidGame.VisualObjects
+{
+    internal abstract class ColissionObject : VisualObject, ICollision
+    {
+        public ColissionObject(Point Position, Point Direction, Size Size) 
+            : base(Position, Direction, Size)
+        {
+        }
+
+        public Rectangle Rect => new Rectangle(_Direction,_Size);
+        public bool CheckCollision(ICollision obj) => Rect.IntersectsWith(obj.Rect);
+
+        public override void Draw(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
